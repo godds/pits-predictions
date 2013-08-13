87eb5c596d11d819bbcb765239608de23490e2e5
godds <godds@scottlogic.co.uk>
2013-08-13 20:30:42 +0100

    first passing at adding in predictions

--- js/controllers.js
+++ js/controllers.js
@@ -4,13 +4,87 @@ function TeamController($scope, table) {
     $scope.teams = table.query();
 }
 
-function PredictionController($scope, predictions) {
-    predictions.query(function(result) {
-        $scope.$apply(function() {
-            $scope.prediction = result;
+function PredictionController($scope, world, predictions) {
+    $scope.standings = [];
+    $scope.currentWorld;
+    var currentPredictions;
+
+    var calculatePremierLeaguePoints = function(league, prediction) {
+        var result = 0;
+        prediction.forEach(function(item, index) {
+            result += Math.abs(index - league.indexOf(item));
         });
+        return result;
+    };
+    var calculateCupPoints = function(results, prediction) {
+        if (!results || !results.length) {
+            return 0;
+        }
+        // TODO 2 4 6
+        return 0;
+    };
+    var calculateTopScorerPoints = function(topScorers, prediction) {
+        if (!topScorers || !topScorers.length) {
+            return 0;
+        }
+        topScorers = topScorers.map(function(player) { return player.player; });
+        var index = topScorers.indexOf(prediction);
+        return index === 0 ? 0
+            : index === 1 || index === 2 ? 2 : 4;
+    };
+    var calculatePersonPoints = function(person, prediction) {
+        return !person || person == prediction ? 0 : 3;
+    };
+    var updateStandings = function() {
+        if (!$scope.currentWorld || !currentPredictions) {
+            return;
+        }
+        $scope.$apply(function() {
+            currentPredictions.forEach(function(item) {
+                var points = {
+                    player: item.get("player"),
+                    premierLeague: calculatePremierLeaguePoints($scope.currentWorld.premierLeague, item.get("premierLeague")),
+                    faCup: calculateCupPoints($scope.currentWorld.faCup, item.get("faCup")),
+                    leagueCup: calculateCupPoints($scope.currentWorld.leagueCup, item.get("leagueCup")),
+                    championsLeague: calculateCupPoints($scope.currentWorld.championsLeague, item.get("championsLeague")),
+                    topScorer: calculateTopScorerPoints($scope.currentWorld.topScorer, item.get("topScorer")),
+                    sackedManager: calculatePersonPoints($scope.currentWorld.sackedManager, item.get("sackedManager")),
+                    playerOfTheYear: calculatePersonPoints($scope.currentWorld.playerOfTheYear, item.get("playerOfTheYear")),
+                    youngPlayerOfTheYear: calculatePersonPoints($scope.currentWorld.youngPlayerOfTheYear, item.get("youngPlayerOfTheYear"))
+                };
+                points.total = points.premierLeague
+                             + points.faCup
+                             + points.leagueCup
+                             + points.championsLeague
+                             + points.topScorer
+                             + points.sackedManager
+                             + points.playerOfTheYear
+                             + points.youngPLayerOfTheYear;
+                $scope.standings.push(points);
+            });
+            $scope.standings.sort(function(a, b) {
+                return -1;
+            });
+        });
+    };
+
+    world.query(function(result) {
+        $scope.currentWorld = result;
+        updateStandings();
     },
     function(error) {
-        alert("sad face");
+        console.log("error fetching world");
     });
+
+    predictions.query(function(result) {
+        currentPredictions = result;
+        updateStandings();
+    },
+    function(error) {
+        console.log("error fetching predictions");
+    });
+}
+
+function PlayerPredictionController($scope) {
+
 }
--- js/services.js
+++ js/services.js
@@ -1,11 +1,6 @@
 "use strict";
 
 angular.module("pp-services", ["ngResource"]).
-    factory("table", function($resource) {
-        return $resource("http://api.statsfc.com/premier-league/table.json",
-                         { key: "free", callback: "JSON_CALLBACK" },
-                         { query: { method: "JSONP", isArray: true } });
-    }).
     factory("predictions", function($resource) {
         var Prediction = Parse.Object.extend("Prediction");
         return {
@@ -17,4 +12,96 @@ angular.module("pp-services", ["ngResource"]).
                 return query.find({ success: success, error: error });
             }
         }
+    }).
+    factory("table", function($resource) {
+        return $resource("http://api.statsfc.com/premier-league/table.json",
+                        { key: "free", callback: "JSON_CALLBACK", cache: true },
+                        { query: { method: "JSONP", isArray: true } });
+    }).
+    factory("faCup", function($resource) {
+        return $resource("http://api.statsfc.com/fa-cup/results.json",
+                         { key: "free", callback: "JSON_CALLBACK", cache: true, limit: 6 },
+                         { query: { method: "JSONP", isArray: true } });
+    }).
+    factory("leagueCup", function($resource) {
+        return $resource("http://api.statsfc.com/league-cup/results.json",
+                         { key: "free", callback: "JSON_CALLBACK", cache: true, limit: 6 },
+                         { query: { method: "JSONP", isArray: true } });
+    }).
+    factory("championsLeague", function() {
+        return {
+            query: function() {
+                return [];
+            }
+        };
+    }).
+    factory("topScorers", function($resource) {
+        return $resource("http://api.statsfc.com/premier-league/top-scorers.json",
+                         { key: "free", callback: "JSON_CALLBACK", cache: true, limit: 6 },
+                         { query: { method: "JSONP", isArray: true } });
+    }).
+    factory("sackedManager", function() {
+        return {
+            query: function() {
+                return null;
+            }
+        };
+    }).
+    factory("playerOfTheYear", function() {
+        return {
+            query: function() {
+                return null;
+            }
+        };
+    }).
+    factory("youngPlayerOfTheYear", function() {
+        return {
+            query: function() {
+                return null;
+            }
+        };
+    }).
+    factory("world", function(table, faCup, leagueCup, championsLeague, topScorers, sackedManager, playerOfTheYear, youngPlayerOfTheYear) {
+        var returned = {
+            premierLeague: false,
+            faCup: false,
+            leagueCup: false,
+            topScorers: false
+        };
+        var result = {};
+        var returnResult = function(success, error) {
+            // TODO handle errors
+            if (returned.premierLeague &&
+                returned.faCup &&
+                returned.leagueCup &&
+                returned.topScorers) {
+                success(result);
+            }
+        }
+
+        return {
+            query: function(success, error) {
+                // TODO handle errors
+                result.championsLeague = championsLeague.query();
+                result.sackedManager = sackedManager.query();
+                result.playerOfTheYear = playerOfTheYear.query();
+                result.youngPlayerOfTheYear = youngPlayerOfTheYear.query();
+                result.premierLeague = table.query(null, function() {
+                    returned.premierLeague = true;
+                    returnResult(success, error);
+                });
+                result.faCup = faCup.query(null, function() {
+                    returned.faCup = true;
+                    returnResult(success, error);
+                });
+                result.leagueCup = leagueCup.query(null, function() {
+                    returned.leagueCup = true;
+                    returnResult(success, error);
+                });
+                result.topScorers = topScorers.query(null, function() {
+                    returned.topScorers = true;
+                    returnResult(success, error);
+                });
+            }
+        };
     });
